c7719e6c6fe3c39023bb926cf8072af5e0e61a94
AlejandroMixteco <alejandro.mixteco.24e@utzmg.edu.mx>
2025-11-25 12:30:14 -0600

    modifcar campos

--- index.js
+++ index.js
@@ -248,7 +248,7 @@ app.post("/api/productos/insertar", upload.fields([
         corto_sep, largo_sep, imagen, suajes_num_suaje, fecha,
         ancho_int, largo_int, grabado, clientes_num_cliente,
         marcas, clave, pegado, descripcion, tipo, producto, guia,
-        clave_material, sat_clave_producto_servicio, sat_clave_unidad, empaque, paquete, cantidad_tarima
+        clave_material, satclaveproductoservicio, satclaveunidad, empaque, paquete, cantidad_tarima
       ) VALUES (
         $1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13,$14,$15,$16,$17,$18,
         $19,$20,$21,$22,$23,$24,$25,$26,$27,$28,$29,$30,$31
